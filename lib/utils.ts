ab408b95f3eea9008ab0cf7fc962a8428f32413a
Rajal7777 <suwalrajal57@gmail.com>
2026-06-23 13:07:21 +0900

    function for 2 digit decimal number

--- lib/utils.ts
+++ lib/utils.ts
@@ -1,5 +1,6 @@
 import { clsx, type ClassValue } from "clsx";
 import { twMerge } from "tailwind-merge";
+import { int } from "zod";
 
 export function cn(...inputs: ClassValue[]) {
     return twMerge(clsx(inputs));
@@ -23,4 +24,13 @@ value: T here the value of T will be whatever the type of <T>
 
 export function convertToPlainObject<T>(value: T): T {
     return JSON.parse(JSON.stringify(value));
+}
+
+//Format number with decimal places
+//padEnd(length, character) adds characters to the end until the string reaches the specified length. used in sensitive number like account no 555*** ***
+
+export function formatNumberWithDecimal(num: number): string {
+    const [int, decimal] = num.toString().split('.');
+    return decimal ? `${int}.${decimal.padEnd(2, '0')}` : `${int}.00`
+
 }
--- lib/validators.ts
+++ lib/validators.ts
@@ -1,15 +1,25 @@
-import { ProductScalarFieldEnum } from './generated/prisma/internal/prismaNamespaceBrowser';
-import { z } from 'zod';
+import { z } from "zod";
+import { formatNumberWithDecimal } from "./utils";
+
+//refine() lets you create your own custom validation rule.
+//syntax refine(conditon, 'Error message)
+const currency = z
+    .string()
+    .refine(
+        (value) => /^\d+(\.\d{2})?$/.test(formatNumberWithDecimal(Number(value))),
+        "Price must have exactly two decimal place",
+    );
 
 //Schema for inserting products
 export const insertProductSchema = z.object({
-    name: z.string().min(3, 'Name must be at least 3 characters'),
-    slug: z.string().min(3, 'Slug must be at least 3 characters'),
-    category: z.string().min(3, 'Category must be at least 3 characters'),
-    brand: z.string().min(3, 'Brand must be at least 3 characters'),
-    description: z.string().min(3, 'Description must be at least 3 characters'),
+    name: z.string().min(3, "Name must be at least 3 characters"),
+    slug: z.string().min(3, "Slug must be at least 3 characters"),
+    category: z.string().min(3, "Category must be at least 3 characters"),
+    brand: z.string().min(3, "Brand must be at least 3 characters"),
+    description: z.string().min(3, "Description must be at least 3 characters"),
     stock: z.coerce.number(), //stcok will come as a string so convert it into the number
-    images: z.array(z.string()).min(1, 'Product must have at least one image'),
+    images: z.array(z.string()).min(1, "Product must have at least one image"),
     isFeateured: z.boolean,
     banner: z.string().nullable(), //optional value
-})
+    price: currency,
+});
