057653b3097c658312e3ee2eb78eaeadc6f3490e
Rajal7777 <suwalrajal57@gmail.com>
2026-06-23 11:23:27 +0900

    merged lib/util.ts

--- lib/actions/product.actions.ts
+++ lib/actions/product.actions.ts
@@ -1,12 +1,13 @@
 'use server';
 //prisma object lets you communicate with database
 import { prisma } from '@/lib/prisma';
-import { convertToPlainObject } from '../constants/utils';
+import { convertToPlainObject } from '../utils';
+
 
 //Get latest products
 export async function getLatestProducts() {
     const data = await prisma.product.findMany({
-        take: 6,
+        take: 4,
         orderBy: { createdAt: 'desc' }
     });
 
--- lib/utils.ts
+++ lib/utils.ts
@@ -1,6 +1,26 @@
-import { clsx, type ClassValue } from "clsx"
-import { twMerge } from "tailwind-merge"
+import { clsx, type ClassValue } from "clsx";
+import { twMerge } from "tailwind-merge";
 
 export function cn(...inputs: ClassValue[]) {
-  return twMerge(clsx(inputs))
+    return twMerge(clsx(inputs));
+}
+
+
+//convert prisma object into a regular JS object
+//here T is the type that will be defined later unknown for now
+//convertToPlainObject("hello")  ->  then T type will be T = string
+/*
+convertToPlainObject({
+    name: "John",
+    age: 25
+})
+    T = {
+    name: string;
+    age: number;
+}
+value: T here the value of T will be whatever the type of <T>
+*/
+
+export function convertToPlainObject<T>(value: T): T {
+    return JSON.parse(JSON.stringify(value));
 }
