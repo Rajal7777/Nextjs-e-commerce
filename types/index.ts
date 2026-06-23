a930f4ae02e30f969314f1ef8f00cf5fcb72b79e
Rajal7777 <suwalrajal57@gmail.com>
2026-06-23 12:38:42 +0900

    added the product type + zod schema types

--- types/index.ts
+++ types/index.ts
@@ -1,6 +1,17 @@
-import { Product } from './../lib/generated/prisma/client';
 import { z } from "zod";
 import { insertProductSchema } from "@/lib/validators";
 
+//z.infer<typeof insertProductSchema> -> creates a TypeScript type from the inserProductschema {schema}
+//typeof means get the types of this variable and we use '&' to combine the type of schema type and extra added types
 
-export type Product = z.infer<typeof insertProductSchema>
+/*
+typeof insertProductSchema	=>Get the type of a variable
+z.infer<> => Convert a Zod schema into a TypeScript type
+& => Combine two types
+*/
+
+export type Product = z.infer<typeof insertProductSchema> & {
+    id: string;
+    rating: number;
+    createdAt: Date;
+}
