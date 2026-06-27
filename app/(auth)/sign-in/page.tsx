966a46ad8d12bab1a7f2dc2328f16bfbbdfd89ea
Rajal7777 <suwalrajal57@gmail.com>
2026-06-28 00:04:35 +0900

    sign in page layout

--- app/(auth)/sign-in/page.tsx
+++ app/(auth)/sign-in/page.tsx
@@ -1,5 +1,8 @@
-import { Card } from "@/components/ui/card";
+import { Card, CardContent, CardDescription, CardHeader, CardTitle } from "@/components/ui/card";
+import { APP_NAME } from "@/lib/constants";
 import { Metadata } from "next";
+import Image from "next/image";
+import Link from "next/link";
 
 
 export const metadata: Metadata = {
@@ -9,7 +12,20 @@ const SignInpage = () => {
     return (
         <div className="w-full max-w-md mx-auto">
             <Card>
+                <CardHeader className="space-y-4">
+            <Link href='/' className="flex-center">
+            <Image src='/images/logo.svg' alt={`${APP_NAME}`} />
+            </Link>
 
+                <CardTitle className="text-center">Sign In</CardTitle>
+                <CardDescription className="text-center">
+                    Sign in to you account
+                </CardDescription>
+                </CardHeader>
+
+                <CardContent className="space-y-4">
+                {/*  Sign in form */}
+                </CardContent>
             </Card>
         </div>
     )
