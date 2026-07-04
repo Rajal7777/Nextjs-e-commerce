0f787ea790096e5d3394b8ebf16d32c30c767fb1
Rajal7777 <suwalrajal57@gmail.com>
2026-07-04 23:49:31 +0900

    store user addressin database after zod validation

--- app/shipping-address/shipping-address.tsx
+++ app/shipping-address/shipping-address.tsx
@@ -24,6 +24,7 @@ import { useRouter } from "next/navigation";
 import { useEffect, useTransition } from "react";
 import { Controller, useForm } from "react-hook-form";
 import { toast } from "sonner";
+import { updateUserAddress } from "@/lib/actions/user.actions";
 import * as z from "zod";
 
 const ShippingAddressForm = ({ address }: { address: ShippingAddress; }) => {
@@ -46,24 +47,20 @@ const ShippingAddressForm = ({ address }: { address: ShippingAddress; }) => {
     }, [isSubmitSuccessful, reset]);
 
     function onSubmit(data: z.infer<typeof shippingAddressSchema>) {
-        console.log('data', data);
-        toast("You submitted the following values:", {
-            description: (
-                <pre className="mt-2 w-[320px] overflow-x-auto rounded-md bg-code p-4 text-code-foreground">
-                    <code>{JSON.stringify(data, null, 2)}</code>
-                </pre>
-            ),
-            position: "bottom-right",
-            classNames: {
-                content: "flex flex-col gap-2",
-            },
-            style: {
-                "--border-radius": "calc(var(--radius)  + 4px)",
-            } as React.CSSProperties,
+        startTransition(async () => {
+            const res = await updateUserAddress(data);
+
+            if (!res.success) {
+                toast.error(<pre className="mt-2 w-[320px] overflow-x-auto rounded-md bg-code p-4 text-code-foreground">
+                    <code>{res.message}</code>
+                </pre>);
+            }
+                return;
         });
+        router.push('/payment-method')
     }
     return (
-        <Card className="w-full sm:max-w-md mx-auto">
+        <Card className="w-full sm:max-w-md  mx-auto ">
             <CardHeader>
                 <CardTitle>Shipping Address Form</CardTitle>
                 <CardDescription>
@@ -151,7 +148,7 @@ const ShippingAddressForm = ({ address }: { address: ShippingAddress; }) => {
                         />
 
                         <Field orientation="responsive">
-                          <Button type="submit" disabled={isPending} form="form-rhf-demo" onClick={() => form.resetField}>
+                            <Button type="submit" disabled={isPending} form="form-rhf-demo" onClick={() => form.resetField}>
                                 {isPending ? (
                                     <Loader className="w-4 h-4 animate-spin" />
                                 ) : (
--- lib/actions/user.actions.ts
+++ lib/actions/user.actions.ts
@@ -1,12 +1,17 @@
 "use server";
 
-import { signInFormSchema, signUpFormSchema } from "../validators";
-import { signIn, signOut } from "@/auth";
+import {
+  shippingAddressSchema,
+  signInFormSchema,
+  signUpFormSchema,
+} from "../validators";
+import { auth, signIn, signOut } from "@/auth";
 import { isRedirectError } from "next/dist/client/components/redirect-error";
 
 import { prisma } from "@/db/prisma";
 import { hashSync } from "bcryptjs";
 import { formatError } from "../utils";
+import { ShippingAddress } from "@/types";
 
 
 //Sign in the user with credentials
@@ -93,4 +98,31 @@ export async function getUserById(userId: string) {
   if (!user) throw new Error("User not found!");
 
   return user;
+}
+
+//Update the user's address
+export async function updateUserAddress(data: ShippingAddress) {
+  try {
+    const session = await auth();
+  console.log("session",session)
+    const currentUser = await prisma.user.findUnique({
+      where: { id: session?.user?.id },
+    });
+
+    if (!currentUser) throw new Error("User not found");
+
+    const address = shippingAddressSchema.parse(data);
+
+    await prisma.user.update({
+      where: { id: currentUser.id },
+      data: { address },
+    });
+
+    return {
+      success: true,
+      message: "User address updated successfully!",
+    };
+  } catch (error) {
+    return { success: false, message: formatError(error) };
+  }
 }
